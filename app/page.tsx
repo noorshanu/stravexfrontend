4fe8bcfe161c4cffe8dc2fc723dd16d58a5a38b1
noor alam <51040978+noorshanu@users.noreply.github.com>
2026-01-28 02:14:58 +0530

    ns

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
   <>
-  <h1 className="text-3xl font-bold underline">Hello World</h1>
-  </>
+            <Navbar />
+            <Hero />
+            <Footer />
+    </>
   );
 }
