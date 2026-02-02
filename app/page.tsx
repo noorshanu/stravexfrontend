8b614f2018ea733c2e1a13db686998dd4cc5b9c8
noor alam <51040978+noorshanu@users.noreply.github.com>
2026-02-02 22:36:04 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
             <Aboutus />
           
           <ScrollCards /> 
-            <CoreProduct />
+            {/* <CoreProduct /> */}
             <Thinker />
             <Footer />
     </>
